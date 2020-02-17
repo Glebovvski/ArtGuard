bb5708b62eb8589f38718f97d53a18df9efde485
glebowski <zadachings@gmail.com>
2020-02-17 16:20:44 +0200

    Added Ibterface ISpace for Exit and Area to inherit from

---
+++ Source/ArtGuard/Space.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Space.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARTGUARD_API USpace : public UObject
+{
+	GENERATED_BODY()
+	
+};
